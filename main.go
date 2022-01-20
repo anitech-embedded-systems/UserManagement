be87887980ade196059b898d3e1ceb95ea87f09d
Animesh Sachan <sachan.animesh@gmail.com>
2022-01-20 13:25:45 +0530

    Revert "Restructured to service architecture, Tested"
    This reverts commit 9630817b7f602d859d405a52b65a04abaa54d919.

--- main.go
+++ main.go
@@ -1,29 +1,17 @@
 package main
 
 import (
-	config "main/Config"
+	dbm "main/Data"
 	server "main/Server"
-	mysqlclient "main/client/mysql"
-	dataimpl "main/impl"
-	"main/service"
 )
 
 func main() {
-
-	cfg := config.Get()
-	sqlclient := mysqlclient.New(cfg)
-
-	defer sqlclient.Close()
-	userdata1, _ := dataimpl.New(sqlclient)
-
-	userservice1, _ := service.New(userdata1)
-
-	serv1, _ := server.New(8080, *userservice1)
-
-	serv1.Start()
-	defer serv1.Stop()
-
-	// wait for sigkill
-	// close all resources
-	// shutdown
+	db := dbm.ConnectDB()
+	if db == nil {
+		return
+	}
+	server.HandleRequest()
+	defer db.Close()
 }
+
+//need to follow layered structure
